8332c128fc287bec4425a3f0562fcd6329a80ef1
Victor Salinas <victorsalinas6@gmail.com>
2023-12-13 13:41:05 -0600

    Did the exersice 3

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace IterationStatements
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace IterationStatements
 {
     public class Program
     {
@@ -27,7 +29,108 @@
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+            PrintNumbers();
+            PrintNumbersBy3();
+            EqualOrNot();
+            EvenOrOdd();
+            PositiveOrNegative();
+            VotingOrNot();
+            CheckingInteger();
+            MultiplicationTable();
+        }
+
+        public static void PrintNumbers()
+        {
+            Console.WriteLine("\tPrinting from 1000 to -1000:\n");
+            for(int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintNumbersBy3()
+        {
+            Console.WriteLine("\tPrinting to 999 from 3 to 3:\n");
+            for (int i = 3; i <= 999; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+
+        public static void EqualOrNot()
+        {
+            Console.WriteLine("\tChecking if a number is equal to another number:\n");
+
+            Console.WriteLine("What´s the first number?");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("What´s the second number?");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine((num1 == num2) ? $"{num1} and {num2} are the same number. Congratulations!\n"
+                                             : $"{num1} and {num2} are not the same number. Please try again!\n");
+        }
+
+        public static void EvenOrOdd()
+        {
+            Console.WriteLine("\tChecking if a number is even or odd:\n");
+
+            Console.WriteLine("What´s the number?");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine((num % 2 == 0) ? $"{num} is an even number. Even you can do it!\n"
+                                             : $"{num} is and odd number. Why are you so Odd?\n");
+        }
+
+        public static void PositiveOrNegative()
+        {
+            Console.WriteLine("\tChecking if a number is positive or negative:\n");
+
+            Console.WriteLine("What´s the number?");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (num == 0)
+                Console.WriteLine("0 is not a positive nor a negative number.");
+            else
+                Console.WriteLine((num > 0) ? $"{num} is a positive number. Life is good!\n"
+                                            : $"{num} is a negative number. Why so sad?\n");
+        }
+
+        public static void VotingOrNot()
+        {
+            Console.WriteLine("\tChecking if you can vote:\n");
+
+            Console.WriteLine("What´s your age?");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine((num >= 18) ? $"You can vote! Be responsible with your power!\n"
+                                          : $"You cannot vote! Please return later in life!\n");
+        }
+
+        public static void CheckingInteger()
+        {
+            Console.WriteLine("\tChecking if the number is between -10 and 10:\n");
+
+            Console.WriteLine("What´s the number?");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine((num >= -10 && num <= 10) ? $"{num} is between -10 and 10. You may live another day!\n"
+                                                       : $"{num} is not between -10 and 10. Please exit through the door.\n");
+        }
+
+        public static void MultiplicationTable()
+        {
+            Console.WriteLine("\tI´m going to give you the multiplication table of a given number\n");
+
+            Console.WriteLine("What´s the number?");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
         }
     }
 }
